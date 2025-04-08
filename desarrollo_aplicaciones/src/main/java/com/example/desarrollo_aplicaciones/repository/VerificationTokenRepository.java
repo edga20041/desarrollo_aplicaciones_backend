de231259c4ef2979f59012cf6535265c07904a3e
calde1104 <felicalde44@gmail.com>
2025-04-08 19:33:58 -0300

    Cambio de url con token a codigo de verificacion

--- desarrollo_aplicaciones/src/main/java/com/example/desarrollo_aplicaciones/repository/VerificationTokenRepository.java
+++ desarrollo_aplicaciones/src/main/java/com/example/desarrollo_aplicaciones/repository/VerificationTokenRepository.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.desarrollo_aplicaciones.entity.User;
 import com.example.desarrollo_aplicaciones.entity.VerificationToken;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
-    Optional<VerificationToken> findByToken(String token);
+    Optional<VerificationToken> findByCode(String code);
+    Optional<VerificationToken> findByUser(User user);
+
 }
