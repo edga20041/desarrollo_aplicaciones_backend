4c7c4e676a26c66dcf84a9b3b4f49b7869848e18
Juanni1 <juanignacioalippi@gmail.com>
2025-04-18 20:12:32 -0300

    Get ruta

--- desarrollo_aplicaciones/src/main/java/com/example/desarrollo_aplicaciones/api/model/RutaResponse.java
+++ desarrollo_aplicaciones/src/main/java/com/example/desarrollo_aplicaciones/api/model/RutaResponse.java
@@ -2,11 +2,27 @@ package com.example.desarrollo_aplicaciones.api.model;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class RutaResponse {
     private Long id;
+
     private String nombre;
+
     private String descripcion;
+
     private String origen;
+
     private String destino;
+
+    private Double latitudOrigen;
+
+    private Double longitudOrigen;
+
+    private Double latitudDestino;
+
+    private Double longitudDestino;
+
+    private LocalDateTime fechaCreacion;
 }
